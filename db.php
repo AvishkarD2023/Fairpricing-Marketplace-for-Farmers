427af81396f7749cd194f44adf46e1fffd060b62
unknown <avidaundkar7@gmail.com>
2025-03-30 01:27:32 +0530

    changes

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
     $serverName = "localhost";
     $userName = "root";
-    $password = "root";
+    $password = "";
     $dbName = "agroculture";
 
     $conn = mysqli_connect($serverName, $userName, $password, $dbName);
